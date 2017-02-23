9f2488665ba0d28fe4958aa8e3d413aa109abdd8
joshuagiles <joshuagiles1993@gmail.com>
2017-02-23 07:42:17 -0600

    Changing cube primitive generation to pre-split the faces
    Maybe I will add a separate function later to produce a fully de-
    duplicated cube vertex buffer but for now it is not as needed as one
    which can generate texture UV coords at creation

--- src/procgl/model_prims.c
+++ src/procgl/model_prims.c
@@ -1,59 +1,76 @@
 #include <GL/glew.h>
 #include "vertex.h"
-#include "shader.h"
 #include "texture.h"
+#include "shader.h"
 #include "model.h"
 
 void pg_model_quad(struct pg_model* model)
 {
     pg_model_init(model);
     pg_model_add_vertex(model, &(struct pg_vert3d) {
-        .pos = { -0.5, 0, 0.5 }, .tex_coord = { 0, 0 }
-    });
-    pg_model_add_vertex(model, &(struct pg_vert3d) {
-        .pos = { 0.5, 0, 0.5 }, .tex_coord = { 0, 0 }
-    });
-    pg_model_add_vertex(model, &(struct pg_vert3d) {
         .pos = { -0.5, 0, -0.5 }, .tex_coord = { 0, 0 }
     });
     pg_model_add_vertex(model, &(struct pg_vert3d) {
-        .pos = { 0.5, 0, -0.5 }, .tex_coord = { 0, 0 }
+        .pos = { -0.5, 0, 0.5 }, .tex_coord = { 0, 1 }
     });
-    pg_model_add_triangle(model, 0, 1, 2);
-    pg_model_add_triangle(model, 3, 1, 2);
+    pg_model_add_vertex(model, &(struct pg_vert3d) {
+        .pos = { 0.5, 0, -0.5 }, .tex_coord = { 1, 0 }
+    });
+    pg_model_add_vertex(model, &(struct pg_vert3d) {
+        .pos = { 0.5, 0, 0.5 }, .tex_coord = { 1, 1 }
+    });
+    pg_model_add_triangle(model, 1, 0, 2);
+    pg_model_add_triangle(model, 1, 2, 3);
 }
 
 void pg_model_cube(struct pg_model* model)
 {
     pg_model_init(model);
-    static const vec3 verts[8] = {
-        { 0.5, 0.5, 0.5 },
-        { -0.5, 0.5, 0.5 },
-        { 0.5, -0.5, 0.5 },
-        { -0.5, -0.5, 0.5 },
-        { 0.5, 0.5, -0.5 },
-        { -0.5, 0.5, -0.5 },
-        { 0.5, -0.5, -0.5 },
-        { -0.5, -0.5, -0.5 } };
-    static const unsigned tris[36] = {
-        0, 1, 2,
-        2, 1, 3,
-        5, 4, 6,
-        5, 6, 7,
-        1, 0, 4,
-        1, 4, 5,
-        2, 3, 6,
-        6, 3, 7,
-        0, 2, 4,
-        4, 2, 6,
-        3, 1, 5,
-        3, 5, 7 };
-    pg_model_reserve_verts(model, 8);
-    pg_model_reserve_tris(model, 12);
-    int i;
-    for(i = 0; i < 8; ++i) {
-        model->verts.data[i] = (struct pg_vert3d){
-            .pos = { verts[i][0], verts[i][1], verts[i][2] } };
+    vec3 face = { 0, 0.5, 0 };
+    vec3 point[4] = { { -0.5, 0, -0.5 },
+                      { -0.5, 0, 0.5 },
+                      { 0.5, 0, -0.5 },
+                      { 0.5, 0, 0.5 } };
+    int i, j;
+    int idx[4];
+    for(i = 0; i < 4; ++i) {
+        for(j = 0; j < 4; ++j) {
+            idx[j] = pg_model_add_vertex(model, &(struct pg_vert3d) {
+                .pos = { point[j][0] + face[0],
+                         point[j][1] + face[1],
+                         point[j][2] + face[2] },
+                .tex_coord = { (j > 1), (j & 1) } });
+            /*  Rotate the vertex a quarter-turn for the next face  */
+            vec3_set(point[j], -point[j][1], point[j][0], point[j][2]);
+        }
+        pg_model_add_triangle(model, idx[0], idx[1], idx[2]);
+        pg_model_add_triangle(model, idx[1], idx[3], idx[2]);
+        /*  Rotate the face vector a quarter-turn so the next face has the
+            correct orientation */
+        vec3_set(face, -face[1], face[0], face[2]);
     }
-    memcpy(model->tris.data, tris, sizeof(tris));
+    vec3_set(face, face[0], -face[2], face[1]);
+    for(j = 0; j < 4; ++j) {
+        /*  Rotate the vertex a quarter-turn for the top face   */
+        vec3_set(point[j], point[j][0], -point[j][2], point[j][1]);
+        idx[j] = pg_model_add_vertex(model, &(struct pg_vert3d) {
+            .pos = { point[j][0] + face[0],
+                     point[j][1] + face[1],
+                     point[j][2] + face[2] },
+            .tex_coord = { (j > 1), (j & 1) } });
+    }
+    pg_model_add_triangle(model, idx[0], idx[1], idx[2]);
+    pg_model_add_triangle(model, idx[1], idx[3], idx[2]);
+    vec3_set(face, face[0], -face[1], -face[2]);
+    for(j = 0; j < 4; ++j) {
+        /* Rotate the vertex a half-turn for the bottom face    */
+        vec3_set(point[j], point[j][0], -point[j][1], -point[j][2]);
+        idx[j] = pg_model_add_vertex(model, &(struct pg_vert3d) {
+            .pos = { point[j][0] + face[0],
+                     point[j][1] + face[1],
+                     point[j][2] + face[2] },
+            .tex_coord = { (j > 1), (j & 1) } });
+    }
+    pg_model_add_triangle(model, idx[0], idx[1], idx[2]);
+    pg_model_add_triangle(model, idx[1], idx[3], idx[2]);    
 }
