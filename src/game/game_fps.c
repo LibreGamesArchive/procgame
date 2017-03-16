48adb574151a315568572504cf32684d43851a16
joshuagiles <joshuagiles1993@gmail.com>
2017-03-16 06:12:21 -0500

    Making the FPS game state accumulate inputs until the next tick
    But still use the between-tick inputs in the draw function prediction. This
    makes the game smooth even at fairly low tick-rates.

--- src/game/game_fps.c
+++ src/game/game_fps.c
@@ -7,30 +7,34 @@ struct fps_game_renderer {
     struct pg_viewer view;
     struct pg_gbuffer gbuf;
     struct pg_shader shader_3d;
+    struct pg_shader shader_text;
 };
 
 struct fps_game_assets {
     struct pg_model floor_model;
     struct pg_texture floor_tex;
+    struct pg_texture font;
 };
 
 struct fps_game_data {
     struct fps_game_renderer rend;
     struct fps_game_assets assets;
     const uint8_t* kb_state;
+    vec2 mouse_motion;
     vec3 player_pos, player_vel;
     vec2 player_dir;
 };
 
 static void fps_game_generate_assets(struct fps_game_data* d);
 
+static void fps_game_update(struct pg_game_state* state);
 static void fps_game_tick(struct pg_game_state* state);
 static void fps_game_draw(struct pg_game_state* state);
 static void fps_game_deinit(void* data);
 
 void fps_game_start(struct pg_game_state* state)
 {
-    pg_game_state_init(state, pg_time(), 60, 3);
+    pg_game_state_init(state, pg_time(), 60, 2);
     struct fps_game_data* d = malloc(sizeof(*d));
     int sw, sh;
     pg_screen_size(&sw, &sh);
@@ -39,26 +43,30 @@ void fps_game_start(struct pg_game_state* state)
     pg_viewer_init(&d->rend.view, (vec3){ 0, 0, 0 }, (vec2){ 0, 0 },
         (vec2){ sw, sh }, (vec2){ 0.1, 200});
     pg_shader_3d(&d->rend.shader_3d);
+    pg_shader_text(&d->rend.shader_text);
     fps_game_generate_assets(d);
     pg_shader_3d_set_texture(&d->rend.shader_3d, &d->assets.floor_tex);
+    pg_shader_text_set_font(&d->rend.shader_text, &d->assets.font);
     d->kb_state = SDL_GetKeyboardState(NULL);
-    vec2_set(d->player_pos, 0, 0);
-    vec2_set(d->player_vel, 0, 0);
+    vec2_set(d->mouse_motion, 0, 0);
+    vec2_set(d->player_dir, 0, 0);
+    vec3_set(d->player_pos, 0, 0, 1);
+    vec3_set(d->player_vel, 0, 0, 0);
     SDL_SetRelativeMouseMode(SDL_TRUE);
     state->data = d;
+    state->update = fps_game_update;
     state->tick = fps_game_tick;
     state->draw = fps_game_draw;
     state->deinit = fps_game_deinit;
 }
 
-static void fps_game_tick(struct pg_game_state* state)
+static void fps_game_update(struct pg_game_state* state)
 {
     struct fps_game_data* d = state->data;
     int mx, my;
     SDL_GetRelativeMouseState(&mx, &my);
-    vec2_set(d->player_dir,
-             d->player_dir[0] - (mx * 0.0005f),
-             d->player_dir[1] - (my * 0.0005f));
+    d->mouse_motion[0] -= mx * 0.0005f;
+    d->mouse_motion[1] -= my * 0.0005f;
     SDL_Event e;
     while(SDL_PollEvent(&e)) {
         if(e.type == SDL_QUIT) state->tick = NULL;
@@ -79,6 +87,15 @@ static void fps_game_tick(struct pg_game_state* state)
         d->player_vel[0] -= 0.1 * cos(d->player_dir[0]);
         d->player_vel[1] -= 0.1 * sin(d->player_dir[0]);
     }
+}
+
+static void fps_game_tick(struct pg_game_state* state)
+{
+    struct fps_game_data* d = state->data;
+    vec2_set(d->player_dir,
+             d->player_dir[0] + d->mouse_motion[0],
+             d->player_dir[1] + d->mouse_motion[1]);
+    vec2_set(d->mouse_motion, 0, 0);
     vec2_add(d->player_pos, d->player_pos, d->player_vel);
     vec2_scale(d->player_vel, d->player_vel, 0.8);
 }
@@ -90,7 +107,9 @@ static void fps_game_draw(struct pg_game_state* state)
     vec2 vel_lerp = { d->player_vel[0] * t, d->player_vel[1] * t };
     vec3 pos = { d->player_pos[0] + vel_lerp[0],
                  d->player_pos[1] + vel_lerp[1], 2 };
-    pg_viewer_set(&d->rend.view, pos, d->player_dir);
+    vec2 dir = { d->player_dir[0] + d->mouse_motion[0],
+                 d->player_dir[1] + d->mouse_motion[1] };
+    pg_viewer_set(&d->rend.view, pos, dir);
     pg_gbuffer_dst(&d->rend.gbuf);
     pg_shader_begin(&d->rend.shader_3d, &d->rend.view);
     pg_model_begin(&d->assets.floor_model);
@@ -99,10 +118,15 @@ static void fps_game_draw(struct pg_game_state* state)
     pg_model_draw(&d->assets.floor_model, &d->rend.shader_3d, model_transform);
     pg_gbuffer_begin_light(&d->rend.gbuf, &d->rend.view);
     pg_gbuffer_draw_light(&d->rend.gbuf,
-        (vec4){ 0, 0, 1, 20 },
+        (vec4){ 0, 0, 0.25, 20 },
         (vec3){ 1, 0.25, 0.25 });
     pg_screen_dst();
     pg_gbuffer_finish(&d->rend.gbuf, (vec3){ 0.1, 0.1, 0.1 });
+    pg_shader_begin(&d->rend.shader_text, NULL);
+    char fps_str[10];
+    snprintf(fps_str, 10, "FPS: %d", (int)pg_framerate());
+    pg_shader_text_write(&d->rend.shader_text, fps_str,
+        (vec2){ 0, 0 }, (vec2){ 8, 8 }, 0.1);
 }
 
 static void fps_game_deinit(void* data)
@@ -117,7 +141,10 @@ static void fps_game_deinit(void* data)
 
 static void fps_game_generate_floor_texture(struct pg_texture* tex)
 {
-    pg_texture_init(tex, 128, 128, 1, 2);
+    float h[128 * 128];
+    struct pg_heightmap hmap = { .map = h, .w = 128, .h = 128 };
+    pg_texture_init(tex, 128, 128);
+    pg_texture_bind(tex, 1, 2);
     int x, y;
     for(x = 0; x < 128; ++x) {
         for(y = 0; y < 128; ++y) {
@@ -127,20 +154,20 @@ static void fps_game_generate_floor_texture(struct pg_texture* tex)
             float s = noise2(x / 8.0f, y / 8.0f) * 0.5 + 0.5;
             float c = s * 32 + 150;
             if(dist < 56) {
-                tex->pixels[x + y * tex->w] =
-                    (struct pg_texture_pixel){ c, c, c, 255 };
-                tex->normals[x + y * tex->w].h = s * 255.0f;
+                hmap.map[x + y * 128] = s;
+                pg_texel_set(tex->diffuse[x + y * 128], c, c, c, 255);
+                pg_texel_set(tex->light[x + y * 128], 0, 0, 0, s * 48 + 128);
             } else {
                 float scale = 1 - ((dist - 56) / 8);
                 c *= scale * 0.5 + 0.5;
                 s *= scale;
-                tex->pixels[x + y * tex->w] =
-                    (struct pg_texture_pixel){ c, c, c, 255 };
-                tex->normals[x + y * tex->w].h = s * 255.0f;
+                hmap.map[x + y * 128] = s;
+                pg_texel_set(tex->diffuse[x + y * 128], c, c, c, 255);
+                pg_texel_set(tex->light[x + y * 128], 0, 0, 0, s * 48);
             }
         }
     }
-    pg_texture_generate_normals(tex, 4);
+    pg_texture_generate_normals(tex, &hmap, 2);
     pg_texture_buffer(tex);
 }
 
@@ -157,5 +184,8 @@ static void fps_game_generate_assets(struct fps_game_data* d)
     pg_model_buffer(&d->assets.floor_model, &d->rend.shader_3d);
     /*  Generating the floor texture    */
     fps_game_generate_floor_texture(&d->assets.floor_tex);
+    pg_texture_init_from_file(&d->assets.font, "font_8x8.png", NULL);
+    pg_texture_set_atlas(&d->assets.font, 8, 8);
+    pg_texture_bind(&d->assets.font, 3, 4);
 }
 
