ab096ba0d17b604e76af5466de182c8167c9eb4b
joshuagiles <joshuagiles1993@gmail.com>
2017-10-10 09:19:33 -0500

    Improving the full-bright texture feature slightly
    Now lights can still add to their final color, their diffuse is just the
    absolute minimum value.

--- src/procgl/shaders/screen_frag.glsl
+++ src/procgl/shaders/screen_frag.glsl
@@ -9,11 +9,10 @@ out vec4 frag_color;
 
 void main()
 {
-    if(texture(norm, f_tex_coord).w == 1.0) {
+    frag_color = vec4(0, 0, 0, 0);
+    if(texture(norm, f_tex_coord).w == 1.0)
         frag_color = texture(color, f_tex_coord);
-    } else {
-        frag_color = vec4(texture(color, f_tex_coord).rgb *
-            (texture(light, f_tex_coord).rgb + ambient_light), 1);
-    }
+    frag_color += vec4(texture(color, f_tex_coord).rgb *
+        (texture(light, f_tex_coord).rgb + ambient_light), 1);
 }
 
