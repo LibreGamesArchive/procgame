c697b9aaa69ea17458237cd2aba2764fc66f9c75
joshuagiles <joshuagiles1993@gmail.com>
2018-01-25 23:06:46 -0600

    Make the text shader draw only one line at a time
    Now also takes a vec2 indicating the direction to advance with each character

--- src/procgl/shaders/text_frag.glsl
+++ src/procgl/shaders/text_frag.glsl
@@ -1,6 +1,6 @@
 #version 330
 
-uniform sampler2D font;
+uniform sampler2D pg_texture_0;
 uniform vec3 normal_3d;
 uniform int is_3d;
 in vec2 f_tex;
@@ -11,10 +11,12 @@ layout(location = 1) out vec4 g_normal;
 
 void main()
 {
-    vec4 f = texture(font, f_tex);
-    g_albedo = f * f_color;
+    vec4 f = texture(pg_texture_0, f_tex);
     if(is_3d == 1) {
         if(f.a < 0.5) discard;
+        g_albedo = f * f_color;
         g_normal = vec4(normal_3d.xyz * 0.5 + 0.5, f_color.a);
+    } else {
+        g_albedo = f * f_color;
     }
 }
--- src/procgl/shaders/text_vert.glsl
+++ src/procgl/shaders/text_vert.glsl
@@ -4,15 +4,17 @@ const vec2 verts[6] =
     vec2[](vec2(0.00001, 0.00001), vec2(0.00001, 0.99999), vec2(0.99999, 0.00001),
            vec2(0.99999, 0.00001), vec2(0.00001, 0.99999), vec2(0.99999, 0.99999));
 
-uniform mat4 transform;
+uniform mat4 pg_matrix_mvp;
 /*  Number of glyphs per row in the texture */
 uniform uint font_pitch;
-/*  Size in UV coords of each glyph */
-uniform vec2 glyph_size;
-/*  32x 64-char individually transformed blocks; packed to uints    */
-uniform uint chars[32 * 16];
-uniform vec4 style[32];
-uniform vec4 color[32];
+/*  Size of each glyph in UV (x,y) and actual units (z,w)   */
+uniform vec4 glyph_size;
+/*  64-char string (packed to uints)    */
+uniform uint chars[16];
+/*  Pos (x, y), Glyph size (z), Spacing (w) */
+uniform vec4 style; 
+uniform vec4 color;
+uniform vec2 dir;
 
 #define UINT_CHAR(c, i) (((c) >> (i * 8)) & uint(0xFF))
 
@@ -23,19 +25,21 @@ void main()
 {
     int vert_idx = int(gl_VertexID % 6);
     int char_idx = int(gl_VertexID / 6);
-    int line_idx = char_idx / 64;
     int uint_idx = char_idx / 4;
     int char_idx_in_uint = char_idx % 4;
-    int char_idx_in_line = char_idx % 64;
     uint ch = UINT_CHAR(chars[uint_idx], char_idx_in_uint);
     uint frame = ch - uint(32);
-    vec4 tx = style[line_idx];
-    vec2 vert = verts[vert_idx];
     vec2 tex_offset = vec2(frame % font_pitch * glyph_size.x,
                            frame / font_pitch * glyph_size.y);
-    f_tex = vert * glyph_size + tex_offset;
-    f_color = color[line_idx];
-    gl_Position = transform *
-        vec4(vert * tx.z + tx.xy + vec2(tx.z * tx.w * char_idx_in_line, 0), 0, 1);
+    float glyph_aspect = glyph_size.z / glyph_size.w;
+    vec2 glyph_offset =
+        vec2(style.z * style.w * glyph_aspect * char_idx * dir.x,
+             style.z * style.w * char_idx * dir.y);
+    vec2 vert = verts[vert_idx];
+    f_tex = vert * glyph_size.xy + tex_offset;
+    vert.x *= glyph_aspect;
+    vert = vert * style.z + style.xy + glyph_offset;
+    f_color = color;
+    gl_Position = pg_matrix_mvp * vec4(vert.xy, 0, 1);
 }
 
